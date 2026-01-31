7958ecb1cc9d5e3e25ed010bc551c81e18bb1705
Maddie <mirtorabi1234@gmail.com>
2026-01-31 16:41:40 -0500

    useState implemented

--- LilGuys/src/App.tsx
+++ LilGuys/src/App.tsx
@@ -1,16 +1,19 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  function sendGuy(){
+  const [isVisible,setVisbile] = useState(true);
+  function sendGuy() {
+    setVisbile(false);
   }
   return (
       <div className="w-180 h-120"> 
         <h1>
           Hello LilGuys!
         </h1>
-        <button bg-blue-500 hover:bg-blue-700
-          onClick={sendGuy}>
-        </button>
+          <button type="button" onClick={sendGuy} className={`bg-blue-300 active:bg-blue-200 ${isVisible ? 'visible' : 'invisible'}`}>
+            Send out the lil guys!
+          </button>
         </div>
 
   );
