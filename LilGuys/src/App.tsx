6e440f6ed66325bf5216f5accb7bd4174b96f2db
Maddie <mirtorabi1234@gmail.com>
2026-01-31 16:10:54 -0500

    initial

---
+++ LilGuys/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div className="w-px-840 h-px-680"> 
+        <h1>
+          Hello LilGuys!
+        </h1>
+      </div>   
+  );
+}
+
+export default App
