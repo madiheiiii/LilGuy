0956bf3ed2cd8468da63e1e4dda43ec9c0cc7a18
Maddie <mirtorabi1234@gmail.com>
2026-02-01 16:18:33 -0500

    updated timer  (still broken)

--- LilGuys/src/App.tsx
+++ LilGuys/src/App.tsx
@@ -3,6 +3,9 @@ import "./App.css";
 import Timer from './components/Timer';
 
 function App() {
+  
+
+  
 
   const [timerHours, setTimerHours] = useState(1);
   const [timerMinutes, setTimerMinutes] = useState(0);
@@ -11,21 +14,36 @@ function App() {
   const [isVisible,setVisible] = useState(true);
 
   let interval = useRef<number | null>(null);
+  const [clickCount, setClickCount] = useState(0);
 
   const startTimer=()=>{
+    clickCount + 1;
     const endTime = Date.now() + 1 * 60 * 60 * 1000;
 
     interval.current = setInterval(() => {
       const now = Date.now();
       const distance = endTime - now;
 
-      if (distance <= 0) {
-        if (interval.current !== null){
-          clearInterval(interval.current);
-        }
-      setTimerHours(0);
-      setTimerMinutes(0);
-      setTimerSeconds(0);
+    if (clickCount > 1){
+      if (interval.current !== null){
+        clearInterval(interval.current!);
+      }
+        setTimerHours(0);
+        setTimerMinutes(0);
+        setTimerSeconds(0);
+        setVisible(false);
+      setClickCount(0);
+      return;
+    }
+
+    if (distance <= 0) {
+     if (interval.current !== null){
+      clearInterval(interval.current);
+    }
+      // setTimerHours(0);
+      // setTimerMinutes(0);
+      // setTimerSeconds(0);
+      // setVisible(false);
       return;
     }
       const hours = Math.floor(distance / (1000 * 60 * 60));
@@ -39,14 +57,25 @@ function App() {
   };
 
   useEffect(() => {
-    startTimer();
     return () => {
       if (interval.current !== null){
           clearInterval(interval.current);
         }
       }
   }, []);
+//   const handleClick = () => {
+//     if (clickCount > 1){
+//         startTimer();
+//         setTimerHours(0);
+//         setTimerMinutes(0);
+//         setTimerSeconds(0);
+//         setVisible(false);
+//       setClickCount(0);
+//     }
+//     clickCount + 1;
 
+//     startTimer();
+// }
   function sendGuy() {
     setVisible(false);
   }
@@ -61,7 +90,7 @@ function App() {
         timerMinutes={timerMinutes} 
         timerSeconds={timerSeconds}
         />
-          <button type="button" onClick={sendGuy} className={`bg-blue-300 active:bg-blue-200`}>
+          <button type="button" onClick={startTimer} className={`bg-blue-300 active:bg-blue-200`}>
             Send out the lil guys!
           </button>
         </div>
